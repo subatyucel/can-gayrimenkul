1a9111173ce8bdcb084fb057a63cc4beb7964865
subat <subat.yucel1@gmail.com>
2026-03-06 17:04:58 +0300

    feat: middleware file changed to proxy

--- proxy.ts
+++ proxy.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 import { jwtVerify } from "jose";
 
-export default async function middleware(request: NextRequest) {
+export default async function proxy(request: NextRequest) {
   const secret = new TextEncoder().encode(process.env.JWT_SECRET);
   const session = request.cookies.get("admin_session");
   const isLoginPage = request.nextUrl.pathname === "/admin/login";
