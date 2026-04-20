8e0eab79b664ccb50cd5d2720fe153432ea620e4
subat <subat.yucel1@gmail.com>
2026-04-20 16:30:51 +0300

    mail components implemented with react-mail

---
+++ lib/mail.tsx
@@ -0,0 +1,31 @@
+import nodemailer from 'nodemailer';
+import { render } from 'react-email';
+import ResetPasswordMail from '@/components/emails/ResetPassword';
+
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: process.env.GMAIL_USER,
+    pass: process.env.GMAIL_APP_PASSWORD,
+  },
+});
+
+export async function sendResetPasswordMail(
+  email: string,
+  token: string,
+  fullName: string,
+) {
+  const resetLink = `${process.env.NEXT_PUBLIC_BASE_URL}/admin/sifremi-unuttum?t=${token}`;
+  const html = await render(
+    <ResetPasswordMail fullName={fullName} resetLink={resetLink} />,
+  );
+
+  console.log(html);
+
+  return transporter.sendMail({
+    from: `"Can Gayrimenkul" <${process.env.GMAIL_USER}>`,
+    to: email,
+    subject: 'Şifre Sıfırlama',
+    html,
+  });
+}
